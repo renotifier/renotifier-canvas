d94989e9e81c3fe106701fe2c673e567b666970f
Janez K <janez.kranjc@gmail.com>
2015-05-30 13:59:08 +0200

    added canvas app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# renotifier-canvas
-A Facebook Canvas app that integrates with ReNotifier. Allows easy sending of notifications to your users.
+Renotifier.com Canvas Demo
+==========================
+
+This demo shows how easy it is to connect a Facebook Canvas app with ReNotifier. In fact you do not even have to do any changes to the codebase of your Facebook Canvas app. Check out the code and you will see that it features zero integration with ReNotifier, however you can still use it to send notifications. Learn more @ https://renotifier.com
+
+You can try out this demo live @
+
+https://renotifier.com/demo/canvas/
+
+
---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>ReNotifier Demo - Canvas</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>   
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+    <div id="fb-root"></div>
+        <div class="container">
+        
+        <div class="row">
+        
+        <div class="col-xs-12">
+
+        <div class="text-center"><a href="https://renotifier.com"><img src="https://renotifier.com/static/img/logo_blue_160px.png" style="width:160px; margin-top:15px;"></a></div>
+
+        <h1 class="text-center">Collect Facebook users' information<br> without even having a landing page!</h1>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+        <br><br>
+        <p class="text-center">By taking users to your Facebook App's Canvas Page, Facebook App Login Button you can:
+        <ol>
+          <li>let users effortlessly subscribe to your content, login, register or download something in 2 mouse clicks</li>
+          <li>capture information from the user's Facebook profile (i.e. email address, first name, last name, gender, etc.)</li>
+          <li>get permission to send Facebook Notifications to the user using <a href="https://renotifier.com">ReNotifier.com</a> (without calling our API)</li>
+        </ol></p>
+        
+        <br>
+          <a href="http://apps.facebook.com/renotifier-canvas" class="btn btn-block btn-primary">Click here to go to the canvas page &raquo;</a>     
+        </div>
+        
+        </div>
+    </body>
+</html>
