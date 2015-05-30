f85d6c01cf03b8d432aea2305f1633fd8d6bbddf
Janez K <janez.kranjc@gmail.com>
2015-05-30 14:33:47 +0200

    added utf8 metatag

--- canvas.php
+++ canvas.php
@@ -46,6 +46,7 @@ if (empty($data["user_id"])) {
 <html>
     <head>
         <title>ReNotifier Demo - Canvas</title>
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>   
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
